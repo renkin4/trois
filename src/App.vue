4076880c021ef9761ff45c876a3ce10b4c217313
Yang <weiyang@studiomultiply.com>
2022-03-31 17:59:10 +0800

    project 3dspace to 2d

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-    <YangArchvizComponent :gltfUrl='GLTFTest' debug ref="yangArchvizComp" :modelValue="screenPositions" @update:modelValue="handleModelWValueChange"/>
+  <div class="ui-container">
+    <div class="ui annotation" v-for="pos in screenPositions" :style="`top: ${pos.y}px; left : ${pos.x}px`">
+
+    </div>
+  </div>
+  <YangArchvizComponent :gltfUrl='GLTFTest' debug ref="yangArchvizComp" @update:modelValue="handleModelWValueChange"/>
 </template>
 
 <script setup lang="ts">
@@ -16,6 +21,7 @@ onMounted(()=>{
 });
 
 let handleModelWValueChange = (value : Vector2[]) => { 
+  screenPositions.value = value
 }
 
 </script>
@@ -27,4 +33,18 @@ body, html {
 canvas {
   display: block;
 }
+
+.annotation{
+    position: absolute;
+    border-radius: 50%;
+    display: inline-block;
+    z-index: 10;
+    cursor: pointer;
+    
+    height: 64px;
+    width: 64px;
+    background-color: #FF00FF; 
+    transform: translate(-50%, -50%);
+}
+
 </style>
